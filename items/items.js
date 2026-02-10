cc33cd3797d735b3607811d98d575aea6836520c
spires77 <levi.remmers@gmx.de>
2026-02-10 20:11:44 +0100

    Add files via upload

---
+++ items/items.js
@@ -0,0 +1,17 @@
+const items = [
+    {
+        link: "items/item1.html",
+        img: "gfx/logo2.png",
+        text: "Test 1"
+    },
+    {
+        link: "store.html",
+        img: "gfx/logo2.png",
+        text: "Test 2"
+    },
+    {
+        link: "store.html",
+        img: "gfx/logo2.png",
+        text: "Test 3"
+    }
+];
